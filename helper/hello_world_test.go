b7016fc516406c943efa367b5d53ca66a9875d48
hilmiyahya19 <hilmiyahya199@gmail.com>
2026-01-08 11:44:44 +0700

    00:25:37 - Menggagalkan Test

--- helper/hello_world_test.go
+++ helper/hello_world_test.go
@@ -1,19 +1,46 @@
 package helper
 
-import "testing"
+import (
+	"fmt"
+	"testing"
+)
 
-func TestHelloWorld(t *testing.T) {
+func TestHelloWorldHilmi(t *testing.T) {
 	result := HelloWorld("Hilmi")
 	if result != "Hello Hilmi" {
 		// error
-		panic("Result is not 'Hello Hilmi'")
+		t.Fail() // melanjutkan menjalankan kode program meskipun error, melanjutkan eksekusi unit test selanjutnya
 	}
+	fmt.Println("TestHelloWorldHilmi Berjalan") // kalo pale Fail maka kode ini akan dieksekusi
 }
 
 func TestHelloWorldYahya(t *testing.T) {
 	result := HelloWorld("Yahya")
 	if result != "Hello Yahya" {
 		// error
-		panic("Result is not 'Hello Yahya'")
+		t.FailNow() // menghentikan menjalankan kode program jika error, melanjutkan eksekusi unit test selanjutnya
 	}
+	fmt.Println("TestHelloWorldYahya Berjalan") // kalo pale FailNow maka kode ini tidak akan dieksekusi
 }
+
+// Error memanggil Fail()
+func TestHelloWorldError(t *testing.T) {
+	result := HelloWorld("Error")
+	if result != "Hello Error" {
+		// error
+		t.Error("Result is not Hello Error") // mencatat error namun tetap melanjutkan eksekusi
+	}
+	fmt.Println("TestHelloWorldError Berjalan (Dieksekusi meskipun error)")
+}
+
+// Fatal memanggil FailNow()
+func TestHelloWorldFatal(t *testing.T) {
+	result := HelloWorld("Fatal")
+	if result != "Hello Fatal" {
+		// error
+		t.Fatal("Result is not Hello Fatal") // mencatat error dan menghentikan eksekusi
+	}
+	fmt.Println("TestHelloWorldFatal Berjalan (Tidak Dieksekusi jika error)")
+}
+
+// lebih baik menggunakan Error & Fatal karena bisa menambahkan pesan error (informasi kenapa unit test gagal)
